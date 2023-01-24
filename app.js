e455f28c0a6f28d2c9b8900dae3c32d9c958540b
Ian <ianbuen@gmail.com>
2023-01-24 11:29:24 +0800

    fix import

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const express = require("express");
 const session = require("express-session"); 
 const mongoose = require("mongoose");
 const cors = require("cors");
+const path = require('path');
 
 const app = express();
 
