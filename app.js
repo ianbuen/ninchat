c9b8b22a970e63da35a54edc5b6650403de93d9b
Ian <ianbuen@gmail.com>
2023-01-24 11:35:18 +0800

    fix paths

--- app.js
+++ app.js
@@ -29,7 +29,7 @@ app.use(require("./routes/index.js"));
 if (process.env.NODE_ENV === 'production' || process.env.NODE_ENV === 'staging') {
     app.use(express.static('frontend/build'));
     app.get('*', (req, res) => {
-      res.sendFile(path.join(__dirname + 'frontend/build/index.html'));
+      res.sendFile(path.join(__dirname + '/frontend/build/index.html'));
     });
 }
 
